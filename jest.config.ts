dd63c545e64a9f4d95489f3cfb5b981ee47f1a17
kaykeeb3 <kaykeloioladev@gmail.com>
2025-06-13 10:19:54 -0300

    chore(test): setup jest cofig;

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'jest'
+import { pathsToModuleNameMapper } from 'ts-jest'
+import { compilerOptions } from './tsconfig.json'
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+}
+
+export default config
