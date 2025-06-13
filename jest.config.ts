8ff32da54056db05cf9547a2d8d0190043e5313c
kaykeeb3 <kaykeloioladev@gmail.com>
2025-06-13 10:35:41 -0300

    chore(test): jest config;

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,4 @@
 import type { Config } from 'jest'
-import { pathsToModuleNameMapper } from 'ts-jest'
-import { compilerOptions } from './tsconfig.json'
 
 const config: Config = {
   moduleFileExtensions: ['js', 'json', 'ts'],
