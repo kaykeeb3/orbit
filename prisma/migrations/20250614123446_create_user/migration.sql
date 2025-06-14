fa6f3176eb8af48c2fa3024c330bd23cd438cb1c
kaykeeb3 <kaykeloioladev@gmail.com>
2025-06-14 09:39:34 -0300

    chore: switch Prisma schema from SQLite to PostgreSQL

--- prisma/migrations/20250614123446_create_user/migration.sql
+++ prisma/migrations/20250614123446_create_user/migration.sql
@@ -1,13 +1,15 @@
 -- CreateTable
 CREATE TABLE "users" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "profilePicture" TEXT,
     "isAdmin" BOOLEAN NOT NULL DEFAULT false,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
